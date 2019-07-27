cb612aeff6ee6a94291e0f3cbd8e2c1c09e39de7
Kyle Jang <kyle.jang@mail.utoronto.ca>
2019-07-27 16:24:31 -0400

    Format recipes

--- script.js
+++ script.js
@@ -2,19 +2,23 @@ const url = 'https://api.edamam.com/search?'
 const queryParams = 'q=';
 const app_id = '&app_id=d4183916';
 const app_key = '&app_key=9b4713aba7e3dc1fa7798b2bfdf237da'
-
-
-const renderResponse = (res) => {
-  var arrayLength = res.hits.length;
-  for (var i = 0; i < arrayLength; i += 3) {
-    var response = JSON.stringify(res.hits[i].recipe);
-    $('#listed-recipes').append(response); 
-  }
-};
-
-const recipesField = document.querySelector('#listed-recipes');
+var count = 0;
 
 $(document).ready(function() {
+  const renderResponse = (res) => {
+    var arrayLength = res.hits.length;
+    for (var i = 0; i < arrayLength; i += 3) {
+      var label = JSON.stringify(res.hits[i].recipe.label);
+      var yield = JSON.stringify(res.hits[i].recipe.yield);
+      var calories = JSON.stringify(res.hits[i].recipe.calories);
+      var weight = JSON.stringify(res.hits[i].recipe.totalWeight);
+      var ingredients = JSON.stringify(res.hits[i].recipe.ingredients);
+      var dietLabels = JSON.stringify(res.hits[i].recipe.dietLabels);
+      recipeNum = count + 1;
+      $('#listed-recipes').append('<div id="recipe' + recipeNum + '"><div class="name">' + label + '</div> <div class="yield">' + yield + '</div> <div class="ingredients">' + ingredients + '</div> <div class="calories">' + calories + '</div><div class="weight">' + weight + '</div><div class="diet-labels">' + dietLabels + '</div></div>');
+      count++;
+    }
+  };
   const getResponse = () => {
     const foodQuery = $('#searchbox').val();
     const endpoint = `${url}${queryParams}${foodQuery}${app_id}${app_key}`;
@@ -34,6 +38,14 @@ $(document).ready(function() {
 
   $('#search').click(function(event) {
       event.preventDefault();
+      if(count > 0) {
+        var i = 1;
+        while(i <= count) {
+          $('#recipe' + i)[0].remove();
+          i++;
+        }
+        count = 0;
+      }
       getResponse();
   })
 });
